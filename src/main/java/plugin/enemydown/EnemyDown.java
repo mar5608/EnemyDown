a5fa7a9677bdfa24724cb2a399d2f8d9f81c3ab9
mar GitUser <home@enunoMacBook-Pro.local>
2023-07-12 07:00:22 +0900

    新規登録 中級編DAY11 動作確認済み

---
+++ src/main/java/plugin/enemydown/EnemyDown.java
@@ -0,0 +1,17 @@
+package plugin.enemydown;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.enemydown.command.EnemyDownCommand;
+
+public final class EnemyDown extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("enemyDown").setExecutor(new EnemyDownCommand());
+    }
+}
