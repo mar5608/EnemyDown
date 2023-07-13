b40d3be604a61ade9301d0e9c662da7f25be808f
mar GitUser <home@enunoMacBook-Pro.local>
2023-07-13 16:56:57 +0900

    中級編DAY15 エラー発生中

--- src/main/java/plugin/enemydown/EnemyDown.java
+++ src/main/java/plugin/enemydown/EnemyDown.java
@@ -6,14 +6,14 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.enemydown.command.EnemyDownCommand;
 
-public final class EnemyDown extends JavaPlugin implements Listener {
+public final class EnemyDown extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        //EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
-        //Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
-        Bukkit.getPluginManager().registerEvents(this, this);
+        EnemyDownCommand enemyDownCommand = new EnemyDownCommand();
+        //Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(enemyDownCommand, this);
         getCommand("enemyDown").setExecutor(new EnemyDownCommand());
     }
 
